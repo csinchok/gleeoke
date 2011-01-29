8c5990143519607266ebe64f8b7655960d1660e5
Chris Sinchok <chris@sinchok.com>
2011-01-28 20:36:33 -0500

    css changes, ranking page

--- src/django-gleeoke/gleeoke/urls.py
+++ src/django-gleeoke/gleeoke/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^vote$', 'gleeoke.views.vote'),
+    (r'^rankings$', 'gleeoke.views.rankings'),
     (r'^$', 'gleeoke.views.choose'),
 )
--- src/django-gleeoke/gleeoke/views.py
+++ src/django-gleeoke/gleeoke/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
 from gleeoke.models import *
 
+from django.db.models import Count
+
 from django.http import HttpResponse
 from django.utils import simplejson
 
@@ -43,3 +45,7 @@ def vote(request):
         json = simplejson.dumps(response)
         return HttpResponse(json, mimetype='application/json')
         
+def rankings(request, template_name='rankings.html'):
+    songs = (Song.objects.exclude(badvotes=None) | Song.objects.exclude(worsevotes=None)).annotate(Count('worsevotes'), Count('badvotes')).order_by('worsevotes__count', '-badvotes__count')[:10]
+    return render_to_response(template_name, {'songs':songs}, context_instance=RequestContext(request))
+    
