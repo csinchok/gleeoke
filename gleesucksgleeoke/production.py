b18a8ef618ba70bd7b565aa975f8921a99849041
Chris Sinchok <chris@sinchok.com>
2011-01-30 14:17:22 -0500

    Adding fabric (but no fabfile)

--- gleesucksgleeoke/production.py
+++ gleesucksgleeoke/production.py
@@ -1,2 +1,8 @@
-
 from gleesucksgleeoke.settings import *
+
+DATABASE_ENGINE = 'mysql'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'csinchok_gleeoke'
+DATABASE_USER = 'csinchok_gleeoke'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'c3668da6'         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
